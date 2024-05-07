9376bbae3f4464fbef89fc02ebcc8c47eab62580
MukizuL <53896777+MukizuL@users.noreply.github.com>
2024-05-07 17:23:52 +0700

    Authorization is made

--- loginwindow.cpp
+++ loginwindow.cpp
@@ -1,14 +1,113 @@
 #include "loginwindow.h"
 #include "./ui_loginwindow.h"
+#include <QMessageBox>
 
 LoginWindow::LoginWindow(QWidget *parent)
-    : QDialog(parent)
-    , ui(new Ui::LoginWindow)
+   : QDialog(parent)
+   , ui(new Ui::LoginWindow)
 {
-    ui->setupUi(this);
+   ui->setupUi(this);
+   db_connect();
 }
 
 LoginWindow::~LoginWindow()
 {
-    delete ui;
+   delete ui;
+   if (main_window_warehouse)
+   {
+      delete main_window_warehouse;
+   }
+   if (main_window_client)
+   {
+      delete main_window_client;
+   }
+   if (main_window_manager)
+   {
+      delete main_window_manager;
+   }
+   db.removeDatabase("Qt");
+}
+
+void LoginWindow::on_signin_clicked()
+{
+   QString login       = ui->login->text();
+   QString password    = ui->password->text();
+   int     permissions = 0;
+
+   if (login == "admin" && password == "admin")
+   {
+      QMessageBox::information(this, "Успех", "Добрый день, " + login);
+      switch (permissions)
+      {
+      case 0:
+         hide();
+         main_window_manager = new MainWindowManager(this);
+         main_window_manager->show();
+         break;
+
+      case 1:
+         hide();
+         main_window_warehouse = new MainWindowWarehouse(this);
+         main_window_warehouse->show();
+         break;
+
+      case 2:
+         hide();
+         main_window_client = new MainWindowClient(this);
+         main_window_client->show();
+         break;
+      }
+   }
+   else
+   {
+      QMessageBox::warning(this, "Ошибка", "Введён неправильный логин/пароль!");
+   }
+}
+
+void LoginWindow::on_invokesql_clicked()
+{
+   QSqlQuery query;
+
+   query.exec("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'");
+
+   if (query.isActive())
+   {
+      qDebug() << "Tables in the database:";
+      while (query.next())
+      {
+         QString tableName = query.value(0).toString();
+         qDebug() << tableName;
+      }
+   }
+   else
+   {
+      qDebug() << "Failed to retrieve tables:" << query.lastError().text();
+   }
+}
+
+void LoginWindow::on_db_reconnect_clicked()
+{
+   db.close();
+   db.open();
+}
+
+void LoginWindow::db_connect()
+{
+   db = QSqlDatabase::addDatabase("QODBC");
+   db.setHostName("127.0.0.1");
+   db.setPort(1433);
+   db.setUserName("admin");
+   db.setPassword("admin");
+   db.setDatabaseName("Qt");
+
+   if (db.open())
+   {
+      ui->db_status->setText("Подключен");
+   }
+   else
+   {
+      ui->db_status->setText("Отключен");
+      QSqlError err = db.lastError();
+      QMessageBox::critical(this, "No", err.text());
+   }
 }
--- loginwindow.h
+++ loginwindow.h
@@ -2,6 +2,12 @@
 #define LOGINWINDOW_H
 
 #include <QDialog>
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+#include "mainwindowclient.h"
+#include "mainwindowmanager.h"
+#include "mainwindowwarehouse.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -11,13 +17,25 @@ QT_END_NAMESPACE
 
 class LoginWindow : public QDialog
 {
-    Q_OBJECT
+   Q_OBJECT
 
 public:
-    LoginWindow(QWidget *parent = nullptr);
-    ~LoginWindow();
+   LoginWindow(QWidget *parent = nullptr);
+   ~LoginWindow();
+   void db_connect();
+
+private slots:
+   void on_signin_clicked();
+
+   void on_invokesql_clicked();
+
+   void on_db_reconnect_clicked();
 
 private:
-    Ui::LoginWindow *ui;
+   Ui::LoginWindow *ui;
+   MainWindowClient *main_window_client       = NULL;
+   MainWindowManager *main_window_manager     = NULL;
+   MainWindowWarehouse *main_window_warehouse = NULL;
+   QSqlDatabase db;
 };
 #endif // LOGINWINDOW_H
