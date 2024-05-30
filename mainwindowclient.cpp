24277b071b39c675adf57e5f0245fa6a4c2c9503
MukizuL <53896777+MukizuL@users.noreply.github.com>
2024-05-30 21:48:37 +0700

    Remove passing of db instance. Changed QSqlTableModel to QSqlQueryModel

--- mainwindowclient.cpp
+++ mainwindowclient.cpp
@@ -2,26 +2,22 @@
 #include "ui_mainwindowclient.h"
 #include "dialog_create_order.h"
 
-MainWindowClient::MainWindowClient(QSqlDatabase database, int user, QWidget *parent)
+MainWindowClient::MainWindowClient(int user, QWidget *parent)
    : QMainWindow(parent)
    , ui(new Ui::MainWindowClient)
 {
    ui->setupUi(this);
-   db           = database;
+   db           = QSqlDatabase::database();
    id_client    = user;
-   orders_model = new QSqlTableModel(this, db);
-   orders_model->setTable("Orders");
-   orders_model->select();
+   orders_model = new QSqlQueryModel;
+   update_model();
    ui->tableView_client->setModel(orders_model);
-   ui->tableView_client->hideColumn(2);
-   ui->tableView_client->hideColumn(4);
 }
 
 MainWindowClient::~MainWindowClient()
 {
    delete ui;
    delete orders_model;
-   db.removeDatabase("Qt");
 }
 
 void MainWindowClient::on_create_orderButton_client_clicked()
@@ -29,6 +25,25 @@ void MainWindowClient::on_create_orderButton_client_clicked()
    QDialog *dialog = new DialogCreateOrder(db, id_client, this);
 
    dialog->exec();
-   orders_model->select();
+   update_model();
    delete dialog;
+}
+
+void MainWindowClient::update_model()
+{
+   QSqlQuery orders(db);
+
+   orders.prepare("SELECT Orders.id AS 'Номер заказа',	CASE CAST(Orders.status AS TEXT)"
+                  "WHEN '0' THEN 'Ожидает оплаты'"
+                  "WHEN '1' THEN 'Оплачен'"
+                  "WHEN '2' THEN 'Собирается'"
+                  "WHEN '3' THEN 'Собран'"
+                  "WHEN '4' THEN 'Отправлен'"
+                  "ELSE 'Неизвестный статус'"
+                  "END AS 'Статус',"
+                  "CAST(Orders.discount AS TEXT) || ' %' AS 'Скидка',"
+                  "CAST(Orders.total AS TEXT) || ' руб.' AS 'Итог'"
+                  "FROM Orders");
+   orders.exec();
+   orders_model->setQuery(std::move(orders));
 }
--- mainwindowclient.h
+++ mainwindowclient.h
@@ -6,6 +6,7 @@
 #include <QSqlError>
 #include <QSqlQuery>
 #include <QSqlTableModel>
+#include <QSqlQueryModel>
 
 namespace Ui {
 class MainWindowClient;
@@ -16,16 +17,18 @@ class MainWindowClient : public QMainWindow
    Q_OBJECT
 
 public:
-   explicit MainWindowClient(QSqlDatabase database, int user, QWidget *parent = nullptr);
+   explicit MainWindowClient(int user, QWidget *parent = nullptr);
    ~MainWindowClient();
 
 private slots:
    void on_create_orderButton_client_clicked();
 
 private:
+   void update_model();
+
    Ui::MainWindowClient *ui;
    QSqlDatabase db;
-   QSqlTableModel *orders_model;
+   QSqlQueryModel *orders_model;
    int id_client;
 };
 
